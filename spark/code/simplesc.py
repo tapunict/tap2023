3f674a2d0d55f2da708ae64cad04413da6561e03
Salvo Nicotra <salvo.nicotra@gmail.com>
2023-05-09 16:18:34 +0200

    Pre Lesson

--- spark/code/simplesc.py
+++ spark/code/simplesc.py
@@ -1,7 +1,8 @@
 import pyspark
-
-conf = pyspark.SparkConf().setAppName('SimpleSC').setMaster('local')
+print("Start")
+conf = pyspark.SparkConf().setAppName('SimpleSC').setMaster('local[8]')
 sc = pyspark.SparkContext(conf=conf)
 lotr1 = sc.textFile("spark/dataset/The Lord Of The Ring 1-The Fellowship Of The Ring_djvu.txt") 
-
+numrows=lotr1.count()
+print("Il numero di righe e' %s" % (numrows))
 sc.stop()
